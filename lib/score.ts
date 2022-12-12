7db9f3ca29b3e34655d0c3e8aba529fd382b58b3
Edward Clark <edwardclark1104@gmail.com>
2022-12-12 11:37:22 +0000

    please

--- lib/score.ts
+++ lib/score.ts
@@ -4,25 +4,26 @@ type Score = {
   [key: string]: number;
 };
 
-let score: Score;
-
-fs.readFile("/score.json", "utf8", (err, data) => {
-  if (err) {
-    console.log(err);
-  } else {
-    score = JSON.parse(data);
-  }
-});
+// let score: Score;
 
 const getScore = () => {
+  let score: Score = {};
+  fs.readFile("/score.json", "utf8", (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      score = JSON.parse(data);
+    }
+  });
   return { ...score };
 };
 
 const setScore = (newScore: Score) => {
-  score = newScore;
-  fs.writeFile("/score.json", JSON.stringify(score), "utf8", (err) => {
+  // score = newScore;
+  fs.writeFile("/score.json", JSON.stringify(newScore), "utf8", (err) => {
     if (err) console.log(err);
   });
+  return newScore;
 };
 
 export { getScore, setScore };
